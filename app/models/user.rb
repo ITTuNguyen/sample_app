74001c88ac9ff51729cc9db1f3fc10c2eb6b9699
Tu Nguyen <nguyenvantudtu@gmail.com>
2017-11-28 11:37:43 +0700

    chapter14

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_secure_password
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   attr_accessor :remember_token, :activation_token, :reset_token
+
   before_save :downcase_email
   before_create :create_activation_digest
 
@@ -9,7 +20,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: Settings.user.maximum_email},
                     format: {with: VALID_EMAIL_REGEX},
                     uniqueness: {case_sensitive: false}
-  has_secure_password
   validates :password, presence: true, length: {minimum: Settings.user.minimum_pass}, allow_nil: true
 
   def self.digest string
@@ -60,6 +70,18 @@ class User < ApplicationRecord
 
   def feed
     Micropost.by_user id
+  end
+
+  def follow other_user
+    following << other_user
+  end
+
+  def unfollow other_user
+    following.delete other_user
+  end
+
+  def following? other_user
+    following.include? other_user
   end
 
   private
